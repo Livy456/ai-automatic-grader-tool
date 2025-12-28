b5d9f1e410879ba4ba0dc70fb225d2fdf97b412a
Livy456 <Marvelfanatic369@gmail.com>
2025-12-28 07:54:40 -0500

    running website

---
+++ ai-agt/src/app/(student)/layout.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+
+export default function StudentLayout({
+    children
+}: Readonly<{ children: ReactNode }>) {
+    return (
+        <>
+            <Navbar />
+            {children}
+        </>
+    )
+}
+
+function Navbar() {
+    return <header className="flex h-12 shadow bg-background z-10">
+        <nav className="flex gap-4 container"> 
+            <Link className="mr-auto text -lg hover:underline px-2" href="/">
+                AI AGT - Student
+            </Link>
+        </nav>
+        
+    </header>;
+}
---
+++ ai-agt/tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'tailwindcss';
+// import { extend } from 'zod/v4/mini';
+
+export default {
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        container: {
+            center: true,
+            padding: '2rem',
+            screens:{
+                sm: '1500px',
+            },
+        },
+        extend: {
+            colors: {
+                background: "var(--backgroud)",
+                primary: '#1a73e8',
+             },
+        },
+    },
+    plugins: [],
+} satisfies Config;
